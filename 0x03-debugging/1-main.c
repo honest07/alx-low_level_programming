4ea6220a784b5427084e7d19f734a7e50faa8c22
honest07 <dzevehonest00@gmail.com>
2023-03-23 19:19:44 -0700

    Like, commen, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point that causes an infinite loop
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*      putchar(i);*/
+	/*}*/
+	printf("Infinite loop avoided! \\i/\n");
+
+	return (0);
+}
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,2 @@
 script that tests for a positive_or_negative() function similar to the one you worked with in an earlier C project.
+script that copies a file and comment out the part of the code that is causing the output to go into an infinite loop.
