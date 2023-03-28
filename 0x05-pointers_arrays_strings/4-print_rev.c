97e83577a7e16b0d3e8fe9649a6713777c6702ec
honest07 <dzevehonest00@gmail.com>
2023-03-28 11:45:10 -0700

    Making corrections

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,7 @@
  * @s: The string to print
  * Return: void
 */
-void print_rev(char * s)
+void print_rev(char *s)
 {
 	int c = 0;
 
@@ -14,7 +14,7 @@ void print_rev(char * s)
 	{
 	c++;
 	}
-	
+
 	for (c -= 1; c >= 0; c--)
 	{
 		putchar(s[c]);
