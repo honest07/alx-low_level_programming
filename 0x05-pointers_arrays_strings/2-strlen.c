7314b96250dbca36a4701e7152c774a8949ba419
honest07 <dzevehonest00@gmail.com>
2023-03-27 17:57:51 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strlen.c -o 2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,10 +2,10 @@
 
 /**
  * _strlen - Entry point that retruns the length of a string.
- * @str: Makes changes
+ * @str: The string to get the length of
  * Return: Always 0
  */
-size_strlen(const char *str)
+size_t _strlen(const char *str)
 {
 	size_t length = 0;
 
