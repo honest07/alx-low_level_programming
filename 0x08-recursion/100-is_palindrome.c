b739f823ab9cb883b9981193bacdf5f64f287999
honest07 <dzevehonest00@gmail.com>
2023-04-06 00:53:53 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anthing

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - Entry point that checks if a string
+ * is a palindrome
+ * @s: sring to reverse
+ * Return: 1 if it is, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the lenght of
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (i + _strlen_recursion(s + 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -7,3 +7,4 @@ script that writes a function that returns the value of a x raised to the power 
 script that writes a function that returns the natural square root of a number.
 script that writes a function that returns 1 if the input integer is a prime number, otherwise return 0
 a funciton that compares tow strings and returns 1 if the strings can be considered identical, otherwise return 0.
+a function that returns 1 if a string is a palindrome and 0 if not.
